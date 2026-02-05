0173e0d7c9ca7256923f5ca6c22b20b8659cb9ae
Inquieto <nicolanki036@gmail.com>
2026-02-05 03:15:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 import express from "express";
 
 const app = express();
-app.use(express.json());
-
 let views = 0;
 
-// rota do contador
 app.get("/contador", (req, res) => {
   views++;
   res.json({ views });
 });
 
-// porta dinâmica (OBRIGATÓRIO no Render)
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Servidor rodando na porta " + PORT);
-});
+app.listen(3000);
