52b905246a16a8980422b9db6659ce9959cb9250
Inquieto <nicolanki036@gmail.com>
2026-02-05 02:41:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,30 @@
-require("dotenv").config();
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 
-app.get("/login", (req, res) => {
-  const url =
-    "https://discord.com/oauth2/authorize" +
-    `?client_id=${process.env.CLIENT_ID}` +
-    "&response_type=code" +
-    "&scope=identify" +
-    `&redirect_uri=${encodeURIComponent(process.env.REDIRECT_URI)}`;
+const USER_ID = "COLE_SEU_USER_ID_AQUI";
 
-  res.redirect(url);
-});
-
-app.get("/callback", async (req, res) => {
-  const code = req.query.code;
-
-  const token = await axios.post(
-    "https://discord.com/api/oauth2/token",
-    new URLSearchParams({
-      client_id: process.env.CLIENT_ID,
-      client_secret: process.env.CLIENT_SECRET,
-      grant_type: "authorization_code",
-      code,
-      redirect_uri: process.env.REDIRECT_URI
-    }),
-    { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
-  );
-
-  const user = await axios.get(
-    "https://discord.com/api/users/@me",
+app.get("/discord", async (req, res) => {
+  const response = await fetch(
+    `https://discord.com/api/v10/users/${USER_ID}`,
     {
       headers: {
-        Authorization: `Bearer ${token.data.access_token}`
+        Authorization: `Bot ${process.env.BOT_TOKEN}`
       }
     }
   );
 
-  res.json(user.data);
+  const user = await response.json();
+
+  res.json({
+    id: user.id,
+    username: user.username,
+    avatar: user.avatar,
+    flags: user.public_flags
+  });
 });
 
 app.listen(3000, () => console.log("Rodando"));
