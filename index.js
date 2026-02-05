541fa3194266751b6d37bc08f4eb79546573a51e
Inquieto <nicolanki036@gmail.com>
2026-02-05 03:03:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from "express";
-import fetch from "node-fetch";
 import cors from "cors";
 
 const app = express();
 app.use(cors());
+
 let views = 0;
+
 app.get("/view", (req, res) => {
   views++;
   res.json({ views });
+});
+
+app.get("/", (req, res) => {
+  res.send("Backend do contador funcionando");
+});
+
+app.listen(3000, () => {
+  console.log("Contador rodando");
 });
